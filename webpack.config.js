84169c271926a8dcc30f7e18a63abab10ad77d68
WEIWAN3 <wei.wang@thoughtworks.com>
2022-02-22 16:05:22 +0800

    :construction_worker: generate readme doc

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
+class GenReadMePlugin {
+    apply(complier) {
+        complier.hooks.done.tap('gen read me', (stats) => {
+            const fs = require('fs');
+            fs.writeFileSync('./lib/README.md', `# drop-handler 链式的处理使用鼠标拖拽事件
+            new Drop().start(([x, y]) => {
+                    // dosomething
+                })
+                .move(() => {})
+                .end(() => {})
+            `)
+        })
+    }
+}
+
 module.exports = {
     mode: 'production',
     entry: './src/index.ts',
@@ -32,6 +47,7 @@ module.exports = {
                     from: path.join(__dirname, 'package.json'), to: path.join(__dirname, 'lib/package.json')
                 }
             ]
-        })
+        }),
+        new GenReadMePlugin()
     ]
 }
